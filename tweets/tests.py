d29fb0cba351b8e3dbf48ed0265b7ae1dbc20930
Kacey Cole <kcole@izeni.com>
2015-05-02 17:07:29 -0600

    Add sample tests based off of parsers

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
+from .tools import UsertagParser, HashtagParser
+
 # Create your tests here.
+class UsertagParserTestCase(TestCase):
+    def __init__(self, *args, **kwargs):
+        self.parser = UsertagParser()
+        super().__init__(*args, **kwargs)
+
+    def test_matches_single_tag(self):
+        text = "@izeni"
+        matches = self.parser.get_matches(text)
+        self.assertEqual(len(matches), 1)
+
+    def test_matches_multiple_tags(self):
+        text = "@izeni @openwest"
+        matches = self.parser.get_matches(text)
+        self.assertEqual(len(matches), 2)
+
+    def test_matches_only_at_sign(self):
+        text = "@openwest #openwest"
+        matches = self.parser.get_matches(text)
+        self.assertEqual(len(matches), 1)
