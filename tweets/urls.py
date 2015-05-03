5cd2fb114d38f7965a7e4ce5497b09a88900840b
Kacey Cole <kcole@izeni.com>
2015-05-02 18:41:11 -0600

    Avoid breaking 404's with default view. Add self and filtered lists

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from .views import MessageList
+from .views import MessageList, MyMessageList, FilteredMessageList
 
 
 urlpatterns = [
-    url(r'^messages/$', MessageList.as_view())
+    url(r'^my-messages/$', MyMessageList.as_view()),
+    url(r'^messages/(?P<username>\w+)/$', FilteredMessageList.as_view()),
+    url(r'^$|^messages/$', MessageList.as_view()),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.utils.translation import ugettext as _
 from django.views.generic import ListView
 
 from .models import Message
@@ -6,3 +8,21 @@ from .models import Message
 class MessageList(ListView):
     template_name = "message_list.html"
     model = Message
+
+
+class MyMessageList(MessageList):
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+
+
+class FilteredMessageList(MessageList):
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(user__username=self.kwargs.get('username'))
+        if not queryset:
+            raise Http404(_('Username not found.'))
+
+        return queryset
