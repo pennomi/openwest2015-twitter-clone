94da79cce8859417cfde221e3f265343b433a3c5
Kacey Cole <kcole@izeni.com>
2015-05-06 00:46:31 -0600

    Adjusted MessageAdmin to make tags readonly

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Hashtag, Message
 
-admin.site.register(HashTag)
-admin.site.register(Message)
+
+class MessageAdmin(admin.ModelAdmin):
+    readonly_fields = ["tagged_users", "hashtags"]
+
+
+admin.site.register(Hashtag)
+admin.site.register(Message, MessageAdmin)
