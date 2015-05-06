db1b33bb8e64e53db644862fd2ff97b3d9e3ff58
Kacey Cole <kcole@izeni.com>
2015-05-06 01:56:27 -0600

    Added login required to new message view

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from .views import MessageList, MyMessageList, FilteredMessageList, CreateMessage
 
@@ -7,5 +8,5 @@ urlpatterns = [
     url(r'^my-messages/$', MyMessageList.as_view(), name="my-messages"),
     url(r'^messages/(?P<username>\w+)/$', FilteredMessageList.as_view()),
     url(r'^$|^messages/$', MessageList.as_view()),
-    url(r'^new/$', CreateMessage.as_view())
+    url(r'^new/$', login_required(CreateMessage.as_view()))
 ]
