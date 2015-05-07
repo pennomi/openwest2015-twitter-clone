f8ad5ac315f8ecdce50b7ba14eeda490d3da22ae
Kacey Cole <kcole@izeni.com>
2015-05-06 23:43:53 -0600

    Expand messages template.

--- tweets/urls.py
+++ tweets/urls.py
@@ -6,7 +6,7 @@ from .views import MessageList, MyMessageList, FilteredMessageList, CreateMessag
 
 urlpatterns = [
     url(r'^my-messages/$', MyMessageList.as_view(), name="my-messages"),
-    url(r'^messages/(?P<username>\w+)/$', FilteredMessageList.as_view()),
+    url(r'^messages/(?P<username>\w+)/$', FilteredMessageList.as_view(), name="user-messages"),
     url(r'^$|^messages/$', MessageList.as_view(), name="index"),
     url(r'^new/$', login_required(CreateMessage.as_view())),
 ]
