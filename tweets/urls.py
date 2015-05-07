ffcbb71f21a7dc749c4fa69c6e347bb383b8d237
Kacey Cole <kcole@izeni.com>
2015-05-06 23:15:47 -0600

    Add name to index

--- tweets/urls.py
+++ tweets/urls.py
@@ -7,6 +7,6 @@ from .views import MessageList, MyMessageList, FilteredMessageList, CreateMessag
 urlpatterns = [
     url(r'^my-messages/$', MyMessageList.as_view(), name="my-messages"),
     url(r'^messages/(?P<username>\w+)/$', FilteredMessageList.as_view()),
-    url(r'^$|^messages/$', MessageList.as_view()),
-    url(r'^new/$', login_required(CreateMessage.as_view()))
+    url(r'^$|^messages/$', MessageList.as_view(), name="index"),
+    url(r'^new/$', login_required(CreateMessage.as_view())),
 ]
