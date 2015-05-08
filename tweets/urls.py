f2464331c3cb703f99ad47f205e8f2f55bcad9af
Kacey Cole <kcole@izeni.com>
2015-05-08 08:25:38 -0600

    Add name to new message route

--- tweets/urls.py
+++ tweets/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^my-messages/$', MyMessageList.as_view(), name="my-messages"),
     url(r'^messages/(?P<username>\w+)/$', FilteredMessageList.as_view(), name="user-messages"),
     url(r'^$|^messages/$', MessageList.as_view(), name="index"),
-    url(r'^new/$', login_required(CreateMessage.as_view())),
+    url(r'^new/$', login_required(CreateMessage.as_view()), name="new-message"),
 ]
